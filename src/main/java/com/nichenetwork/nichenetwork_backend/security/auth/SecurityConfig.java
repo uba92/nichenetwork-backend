7f7dd6f954cab0370969ec114c7897fd606256b6
Umberto Tramontano <u.tramontano92@libero.it>
2025-03-26 16:28:05 +0100

    fix CORSConfig

--- src/main/java/com/nichenetwork/nichenetwork_backend/security/auth/SecurityConfig.java
+++ src/main/java/com/nichenetwork/nichenetwork_backend/security/auth/SecurityConfig.java
@@ -77,7 +77,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+        configuration.setAllowedOrigins(List.of("http://localhost:5173", "https://niche-network-app.netlify.app"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("Authorization", "Cache-Control", "Content-Type"));
         configuration.setAllowCredentials(false);
